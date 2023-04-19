a72232dbbdbcf3910de4ceb467cb9e4fa66fecd9
dnfebri <dwinofebri@gmail.com>
2023-04-20 00:58:03 +0700

    update: auth and user

--- controllers/Auth.js
+++ controllers/Auth.js
@@ -3,75 +3,77 @@ import bcrypt from "bcrypt";
 import jwt from "jsonwebtoken";
 import { responseJson } from "../helper/Respont.js";
 import { Op } from "sequelize";
+import { uploadImage } from "../helper/UploadImage.js";
 
-export const Login = async(req, res) => {
-  const {username, email, password } = req.body;
-  if (!username && !email) { 
-    return res.status(400).json(responseJson(
-      false, 
-      "Body Content {username, Email, password}"
-    )) 
-  };
+export const Login = async (req, res) => {
+  const { username, email, password } = req.body;
+  if (!username && !email) {
+    return res
+      .status(400)
+      .json(responseJson(false, "Body Content {username, Email, password}"));
+  }
   const reqAuth = email ?? username;
   try {
     const user = await User.findOne({
       where: {
-        [Op.or]: [{ username: reqAuth }, { email: reqAuth }]
-      }
+        [Op.or]: [{ username: reqAuth }, { email: reqAuth }],
+      },
     });
-    if (!user) return res.status(400).json(
-      responseJson(
-        false, "User Not Found"
-      )
-    );
+    if (!user)
+      return res.status(400).json(responseJson(false, "User Not Found"));
     const match = bcrypt.compareSync(password, user.password);
-    if(!match) return res.status(400).json(responseJson(
-      false, "Wrong Password!"
-    ));
+    if (!match)
+      return res.status(400).json(responseJson(false, "Wrong Password!"));
     const token = jwt.sign(
       {
-        id: user.id, 
-        uuid: user.uuid, 
-        name: user.name, 
-        username: user.username, 
-        email: user.email
-      }, 
-      process.env.TOKEN_SECRET, 
+        id: user.id,
+        uuid: user.uuid,
+        name: user.name,
+        username: user.username,
+        email: user.email,
+      },
+      process.env.TOKEN_SECRET,
       { expiresIn: 60 * 60 * 8 } // expired Token
     );
-    return res.status(200).json(
-      responseJson(
-        true, "Successfuly Login", {token}
-      )
-    );
-  } catch (error) {
-    
-  }
-}
+    return res
+      .status(200)
+      .json(responseJson(true, "Successfuly Login", { token }));
+  } catch (error) {}
+};
 
-export const Register = async(req, res) => {
-  const {name, username, email, password, confPassword, photo} = req.body;
-  if (!name || !username || !email || !password || !confPassword) { 
-    return res.status(400).json(responseJson(
-      false, 
-      "Body Content {name, email, password, confPassword}"
-    )) 
-  };
+export const Register = async (req, res) => {
+  const { name, username, email, password, confPassword } = req.body;
+  if (!name || !username || !email || !password || !confPassword) {
+    return res
+      .status(400)
+      .json(
+        responseJson(
+          false,
+          "Body Content {name, email, password, confPassword}"
+        )
+      );
+  }
   const user = await User.findOne({
     where: {
-      [Op.or]: [{ username: username }, { email: username }]
-    }
+      [Op.or]: [{ username: username }, { email: username }],
+    },
   });
-  if (user) return res.status(401).json(
-    responseJson(
-      false, "Username or Email is Already Registered"
-    )
-  );
-  if (password !== confPassword) return res.status(401).json(
-    responseJson(
-      false, "Password dan confirm password tidak cocok"
-    )
-  );
+  if (user)
+    return res
+      .status(401)
+      .json(responseJson(false, "Username or Email is Already Registered"));
+  if (password !== confPassword)
+    return res
+      .status(401)
+      .json(responseJson(false, "Password dan confirm password tidak cocok"));
+  let photo = "https://picsum.photos/200";
+  if (req.files) {
+    const uploadPhoto = uploadImage(req.files, username);
+    if (!uploadPhoto.status)
+      return res
+        .status(422)
+        .json(responseJson(uploadPhoto.status, uploadPhoto.message));
+  }
   try {
     const salt = bcrypt.genSaltSync();
     const hashPassword = bcrypt.hashSync(password, salt);
@@ -80,46 +82,39 @@ export const Register = async(req, res) => {
       username: username,
       email: email,
       password: hashPassword,
-      photo: photo ?? "https://picsum.photos/200",
+      photo: photo,
     });
     res.status(201).json(responseJson(true, "Register Success", userCreate));
   } catch (error) {
     res.status(400).json(responseJson(false, error.massage));
   }
-}
+};
 
-export const Me = async(req, res) => {
-  if (!req.uuid) { //wrong UUID
-    return res.status(401).json(
-      responseJson(
-        false, "wrong UUID"
-      )
-    );
+export const Me = async (req, res) => {
+  if (!req.uuid) {
+    //wrong UUID
+    return res.status(401).json(responseJson(false, "wrong UUID"));
   }
   const user = await User.findOne({
-    attributes:[
-      'uuid', 
-      'name', 
-      'username', 
-      'email', 
-      'photo', 
-      'createdAt', 
-      'updatedAt'
+    attributes: [
+      "uuid",
+      "name",
+      "username",
+      "email",
+      "photo",
+      "createdAt",
+      "updatedAt",
     ],
     where: {
-      uuid: req.uuid
-    }
+      uuid: req.uuid,
+    },
   });
-  if(!user) return res.status(404).json(
-    responseJson(
-      false, "User Not Found"
-    )
-  );
-  return res.status(200).json(
-    responseJson(true, "Successfully Get User", user)
-  );
-}
+  if (!user) return res.status(404).json(responseJson(false, "User Not Found"));
+  return res
+    .status(200)
+    .json(responseJson(true, "Successfully Get User", user));
+};
 
-export const logOut = async(req, res) => {
+export const logOut = async (req, res) => {
   res.status(200).json(responseJson(true, "Logout Success"));
-}
+};
--- middleware/Verify.js
+++ middleware/Verify.js
@@ -2,24 +2,20 @@ import jwt from "jsonwebtoken";
 import { responseJson } from "../helper/Respont.js";
 
 export const verifyToken = (req, res, next) => {
-  if(!req.header('authorization')) return res.status(401).json(
-    responseJson(
-      false, "'Authorization Bearer' Token not detected"
-    )
-  );
-  const token = req.header('authorization').split(" ")[1];
-  if(!token) return res.status(401).json(responseJson(
-    false, "Token not detected"
-  ));
+  if (!req.header("authorization"))
+    return res
+      .status(401)
+      .json(responseJson(false, "'Authorization Bearer' Token not detected"));
+  const token = req.header("authorization").split(" ")[1];
+  if (!token)
+    return res.status(401).json(responseJson(false, "Token not detected"));
   jwt.verify(token, process.env.TOKEN_SECRET, (err, decoded) => {
-    if(err) return res.status(403).json(responseJson(
-      false, err.message
-    ));
+    if (err) return res.status(403).json(responseJson(false, err.message));
     req.userId = decoded.id;
     req.uuid = decoded.uuid;
     next();
-  })
-}
+  });
+};
 
 // export const verifySession = (req, res, next) => {
 //   const token = req.headers.token;
