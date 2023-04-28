4c9fc9879853de72377b66ad93e39920ab8709c8
dnfebri <dwinofebri@gmail.com>
2023-04-28 13:16:57 +0700

    update bodyParser

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ import cors from "cors";
 import dotenv from "dotenv";
 import cookieParser from "cookie-parser";
 import FileUpload from "express-fileupload";
+import bodyParser from "body-parser";
 
 import indexRoute from "./routes/index.js";
 
@@ -29,6 +30,8 @@ dotenv.config();
 
 const app = express();
 
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
 app.use(cookieParser());
 app.use(cors());
 app.use(FileUpload());
